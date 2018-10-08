a01c4bbf980e8b2a54f44e68785f46eb5b02dc1a
rameshz23 <rameshz23@yahoo.com>
2018-10-08 15:35:17 +0530

    Create deletejob.groovy

---
+++ deletejob.groovy
@@ -0,0 +1,10 @@
+import jenkins.model.*
+
+def matchedJobs = Jenkins.instance.items.findAll { job ->
+    job.name =~ /RR_*/
+}
+    
+matchedJobs.each { job ->
+    println job.name
+    //job.delete()
+}
