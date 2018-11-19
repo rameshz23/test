f9f4eb7302958e894185b390d0799ac8de6aae38
rameshz23 <rameshz23@yahoo.com>
2018-11-19 18:50:49 +0530

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -2,7 +2,7 @@ import hudson.model.Result
 import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
 
 	
-def call(cicdProjects) = [
+def cicdProjects = [
 
  [application_platform: 'Test',application_name:'smp',scm: 'git@git.drfirst.com:devops-se/chef-dev-rcopia.git',dev:'devuda',qa:'devuda',sysOps:'devuda',label:'slave1'], 
  
